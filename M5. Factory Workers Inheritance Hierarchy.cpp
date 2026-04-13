37b61985f2d75ed31b043ef23a3274fc7d17f4ba
sterl <sterlingrodgers2007@gmail.com>
2026-04-13 11:57:54 -0400

    Add project files.

---
+++ M5. Factory Workers Inheritance Hierarchy.cpp
@@ -0,0 +1,12 @@
+// M5. Factory Workers Inheritance Hierarchy.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+}
+
+
